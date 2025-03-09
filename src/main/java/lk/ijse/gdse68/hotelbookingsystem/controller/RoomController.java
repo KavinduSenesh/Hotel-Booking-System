e38aaecbbf760bcf93c644813f617a72bad54313
Kavindu Senesh <ksenesh5@gmail.com>
2025-03-09 22:14:19 +0530

    feat: add endpoint to retrieve distinct room types and implement service method

--- src/main/java/lk/ijse/gdse68/hotelbookingsystem/controller/RoomController.java
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/controller/RoomController.java
@@ -5,19 +5,18 @@ import lk.ijse.gdse68.hotelbookingsystem.response.RoomResponse;
 import lk.ijse.gdse68.hotelbookingsystem.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/rooms")
+@CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
 
     private final RoomService roomService;
@@ -31,5 +30,10 @@ public class RoomController {
         Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get/room-types")
+    public List<String> getRoomTypes(){
+        return roomService.getAllRoomTypes();
     }
 }
--- src/main/java/lk/ijse/gdse68/hotelbookingsystem/repository/RoomRepository.java
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/repository/RoomRepository.java
@@ -2,7 +2,12 @@ package lk.ijse.gdse68.hotelbookingsystem.repository;
 
 import lk.ijse.gdse68.hotelbookingsystem.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
-   
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
