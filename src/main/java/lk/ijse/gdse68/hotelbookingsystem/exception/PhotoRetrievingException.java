088df5133e6859145ccd327cdb5f806dd530b003
Kavindu Senesh <ksenesh5@gmail.com>
2025-03-10 15:45:54 +0530

    feat: add custom exception classes for photo retrieval and resource not found scenarios

---
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/exception/PhotoRetrievingException.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse68.hotelbookingsystem.exception;
+
+public class PhotoRetrievingException {
+}
---
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse68.hotelbookingsystem.exception;
+
+public class ResourceNotFoundException extends Throwable {
+    public ResourceNotFoundException(String s) {
+    }
+}
