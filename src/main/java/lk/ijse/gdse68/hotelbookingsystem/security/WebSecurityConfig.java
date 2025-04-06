f0a50f45cec7c3bf50043e14bbb7723359698ad8
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-06 12:24:36 +0530

    feat: implement JWT authentication filter and utility methods for token handling

--- src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/WebSecurityConfig.java
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse68.hotelbookingsystem.security;
 
+import lk.ijse.gdse68.hotelbookingsystem.security.jwt.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,5 +11,10 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
     }
 }
--- src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/jwt/JwtUtils.java
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/jwt/JwtUtils.java
@@ -10,11 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
@@ -41,6 +43,13 @@ public class JwtUtils {
 
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token){
