e554f25ec9bcdcada226a3dc9c785c74c83daee2
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-11 21:59:32 +0530

    feat: add JwtAuthEntryPoint for handling unauthorized access and update WebSecurityConfig for authentication management

--- src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/WebSecurityConfig.java
+++ src/main/java/lk/ijse/gdse68/hotelbookingsystem/security/WebSecurityConfig.java
@@ -1,13 +1,26 @@
 package lk.ijse.gdse68.hotelbookingsystem.security;
 
 import lk.ijse.gdse68.hotelbookingsystem.security.jwt.AuthTokenFilter;
+import lk.ijse.gdse68.hotelbookingsystem.security.jwt.JwtAuthEntryPoint;
+import lk.ijse.gdse68.hotelbookingsystem.security.user.HotelUserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class WebSecurityConfig {
+
+    private final HotelUserDetailService userDetailService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -17,4 +30,19 @@ public class WebSecurityConfig {
     public AuthTokenFilter authTokenFilter() {
         return new AuthTokenFilter();
     }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+
 }
